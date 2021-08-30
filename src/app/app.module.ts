573e469da9f901ea30978f0b51a5f8b0428dab5f
Jose-Santiesteban <jose.santiesteban@clearminds-it.com>
2021-08-29 22:03:33 -0500

    con las rutas creadas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppComponent } from './app.component';
 
 
 import { SharedModule } from './shared/shared.module';
+import { AppRouterModule } from './app-router.module';
+
 
 @NgModule({
   declarations: [
@@ -12,7 +14,8 @@ import { SharedModule } from './shared/shared.module';
   ],
   imports: [
     BrowserModule,
-    SharedModule
+    SharedModule,
+    AppRouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
